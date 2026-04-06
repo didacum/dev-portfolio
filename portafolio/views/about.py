f9c5db95ec5d31ac041ebc31c22bcc49cd042e58
didacum <diegosescribano@gmail.com>
2026-04-06 21:03:44 +0200

    feat: add responsive spacing to about view

--- portafolio/views/about.py
+++ portafolio/views/about.py
@@ -1,9 +1,11 @@
 import reflex as rx
 from portafolio.components.heading import heading
+from portafolio.styles.styles import Size
 
 
 def about(description: str) -> rx.Component:
     return rx.vstack(
         heading("Sobre mí"),
-        rx.text(description)
+        rx.text(description),
+        spacing=[Size.SMALL.value, Size.DEFAULT.value]
     )
